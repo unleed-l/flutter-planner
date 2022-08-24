6864ece83616e5c06aa790a6d108e3a39cc2e485
unleed-l <uendellima54@gmail.com>
2022-08-24 14:57:09 -0300

    final commit

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -40,65 +40,10 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    10,
-                  ),
-                ),
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: themeContext.colorScheme.primary,
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text(
-                          'R\$${tr.value}',
-                          style: const TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: themeContext.textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat(DateFormat.YEAR_MONTH_DAY, 'pt_Br')
-                        .format(tr.date),
-                    style: themeContext.textTheme.headline4,
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 480
-                      ? TextButton.icon(
-                          onPressed: () => onDelete(tr.id),
-                          icon: Icon(
-                            Icons.delete,
-                            color: themeContext.colorScheme.secondary,
-                          ),
-                          label: Text(
-                            'Excluir',
-                            style: TextStyle(
-                              color: themeContext.colorScheme.secondary,
-                            ),
-                          ),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () => onDelete(tr.id),
-                          color: themeContext.colorScheme.secondary,
-                        ),
-                ),
+              return TransactionItem(
+                themeContext: themeContext,
+                tr: tr,
+                onDelete: onDelete,
               );
             },
           );
